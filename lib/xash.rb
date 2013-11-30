ce25d4909d55ff0bd323a838c259d4e76897730e
long-long-float <niinikazuki@yahoo.co.jp>
2013-12-01 00:32:13 +0900

    add samples

--- lib/xash.rb
+++ lib/xash.rb
@@ -6,5 +6,21 @@ module XASH
 
     def eval(code)
         code = Roconv.convert(code)
+
+        code.each do |expr|
+            case expr
+            when Hash
+                case expr.size
+                when 1
+                    #function call
+                    expr = expr.to_a
+                    func_name, arg = expr[0]
+                    case func_name
+                    when 'puts'
+                        puts arg
+                    end
+                end
+            end
+        end
     end
 end
