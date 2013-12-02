e52705e49a5a9b6fef2fe98f7260610b8cdd79b0
long-long-float <niinikazuki@yahoo.co.jp>
2013-12-02 12:57:10 +0900

    implemented for

--- lib/xash.rb
+++ lib/xash.rb
@@ -1,26 +1,49 @@
 require "xash/version"
 require 'roconv'
+require 'pp'
 
 module XASH
     extend self
 
+    def eval_lambda(lambda, *args)
+        lambda_args, *exprs = lambda['do']
+        exprs.each{|expr| eval_expr(expr) }
+    end
+
+    def eval_expr(expr)
+        case expr
+        when Array
+            expr.each{ |e| eval_expr(e) }
+        when Hash
+            k, v = expr.to_a[0]
+            case k
+            when 'puts'
+                v = eval_expr(v)
+                puts v
+            #pseudo functions
+            when 'for'
+                #check_arg(v, 2) or check_arg(v, :collection, :lambda)
+                v = eval_expr(v)
+                collection, func = v[0], v[1]
+                #only range
+                collection = [*collection]
+
+                collection.each do |e|
+                    eval_lambda(func, e)
+                end
+            when 'do'
+                expr
+            else
+                puts "#{k} called!"
+            end
+        else #primitives
+            expr
+        end
+    end
+
     def eval(code)
         code = Roconv.convert(code)
-
-        code.each do |expr|
-            case expr
-            when Hash
-                case expr.size
-                when 1
-                    #function call
-                    expr = expr.to_a
-                    func_name, arg = expr[0]
-                    case func_name
-                    when 'puts'
-                        puts arg
-                    end
-                end
-            end
-        end
+        pp code
+        eval_expr(code)
     end
 end
