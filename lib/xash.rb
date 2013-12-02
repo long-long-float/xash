56790ea344a23318144f08632a50b76328b175c2
long-long-float <niinikazuki@yahoo.co.jp>
2013-12-02 19:24:05 +0900

    modified for sample

--- lib/xash.rb
+++ lib/xash.rb
@@ -7,7 +7,7 @@ module XASH
     class UndefinedFunctionError < StandardError
     end
 
-    class UndefinedLocalVariable < StandardError
+    class UndefinedLocalVariableError < StandardError
     end
 
     class TypeError < StandardError
@@ -18,7 +18,7 @@ module XASH
             def get_local_variable(name)
                 idx = lambda[0].index(name)
                 unless idx
-                    raise UndefinedLocalVariable, "undefined local variable `#{name}`"
+                    raise UndefinedLocalVariableError, "undefined local variable `#{name}`"
                 end
                 lambda_args[idx]
             end
@@ -37,9 +37,11 @@ module XASH
         end
 
         def initialize
-            @context = nil
+            @context_stack = []
+
             @function_table = {
                 'puts' => wrap_pseudo_function(['object'], '__puts'),
+                'print' => wrap_pseudo_function(['object'], '__print'),
                 'for' => wrap_pseudo_function(['collection', 'function'], '__for')
             }
         end
@@ -49,21 +51,50 @@ module XASH
             lambda_args, *exprs = lambda
 
             args = [args] unless args.class == Array
-            @context = Context.new(lambda, args)
+
+            @context_stack.push(Context.new(lambda, args))
 
             exprs.each{|expr| eval_expr(expr) }
+
+            @context_stack.pop
+        end
+
+        def check_args(args, *types)
+            args.zip(types) do |arg, type|
+               check_arg(arg, type) 
+            end
+            nil
+        end
+
+        def check_arg(arg, type)
+            raise TypeError, "`#{arg}` is not `#{type}`" unless self.send("#{type}?", arg)
+            nil
+        end
+
+        def array?(array)
+            array.class == Array
+        end
+
+        def object?(object)
+            object.class == Hash
         end
 
         def lambda?(lambda)
-            lambda.to_a[0] == 'do'
+            lambda.to_a[0][0] == 'do'
+        end
+
+        def collection?(collection)
+            collection.class == Range or collection.class == String or collection.respond_to? :to_a
         end
 
         def to_collection(collection)
             case collection
             when Range
                 [*collection]
+            when String
+                collection.each_char.to_a
             else
-                raise TypeError, "`#{collection}` is not collection" unless collection.respond_to? :to_a
+                raise TypeError, "`#{collection}` is not collection" unless collection? collection
                 collection.to_a
             end
         end
@@ -75,16 +106,22 @@ module XASH
             when Hash
                 k, v = expr.to_a[0]
 
-                if k != 'do'
-                    k = eval_expr(k)
+                no_eval = %w(do object)
+
+                k = eval_expr(k)
+                unless no_eval.index(k)
                     v = eval_expr(v)
                 end
 
                 case k
-                when '__puts' #pseudo functions
+                #pseudo functions
+                when '__puts'
                     puts v
+                when '__print'
+                    print v.join
                 when '__for'
-                    #check_arg(v, 2) or check_arg(v, :collection, :lambda)
+                    check_args(v, :collection, :lambda)
+
                     collection, lambda = v[0], v[1]
 
                     collection = to_collection(collection)
@@ -92,11 +129,21 @@ module XASH
                     collection.each do |e|
                         eval_lambda(lambda, e)
                     end
+
+                #literals
+                when 'array'
+                    check_arg(v, :array)
+                    v
+                when 'object'
+                    check_arg(v, :object)
+                    v
                 when 'do' #lambda
-                    expr #lazy evaluation
+                    expr #for lazy evaluation
+
+                #function call
                 when ->(k) { k.class == Hash and lambda?(k) } #lambda call
                     eval_lambda(k, v)
-                else #other functions
+                else #others
                     unless @function_table.key? k
                         raise UndefinedFunctionError, "called undefiend function `#{k}`"
                     end
@@ -105,7 +152,7 @@ module XASH
             when ->(expr){ expr.class == String and expr =~ /\$(\w+)/ }
                 #local variables
                 var_name = $1
-                @context.get_local_variable(var_name)
+                @context_stack.last.get_local_variable(var_name)
             else
                 #primitives
                 expr
