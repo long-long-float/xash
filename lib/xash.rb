546fcd7825717fdee781c410cfecf475e817b036
long-long-float <niinikazuki@yahoo.co.jp>
2013-12-02 17:42:15 +0900

    wrap pseudo functions

--- lib/xash.rb
+++ lib/xash.rb
@@ -10,54 +10,82 @@ module XASH
                 idx = lambda[0].index(name)
                 unless idx
                     #TODO raise exception
+                    raise "local variable #{name} not found!"
                 end
                 lambda_args[idx]
             end
         end
 
-        def initialize
-            @context = nil
+        def call_function(name, args)
+            { name => args }
         end
 
-        def eval_lambda(lambda, *args)
+        def make_lambda(args, *exprs)
+            { 'do' => [args, *exprs] }
+        end
+
+        def wrap_pseudo_function(args, realname)
+            make_lambda(args, [ call_function(realname, args.map{|arg| "$#{arg}"}) ])
+        end
+
+        def initialize
+            @context = nil
+            @function_table = {
+                'puts' => wrap_pseudo_function(['object'], '__puts'),
+                'for' => wrap_pseudo_function(['collection', 'function'], '__for')
+            }
+        end
+
+        def eval_lambda(lambda, args)
             lambda = lambda['do']
             lambda_args, *exprs = lambda
 
+            args = [args] unless args.class == Array
             @context = Context.new(lambda, args)
 
             exprs.each{|expr| eval_expr(expr) }
         end
 
+        def to_collection(collection)
+            case collection
+            when Range
+                [*collection]
+            else
+                collection.to_a
+            end
+        end
+
         def eval_expr(expr)
             case expr
             when Array
-                expr.each{ |e| eval_expr(e) }
+                expr.map{ |e| eval_expr(e) }
             when Hash
                 k, v = expr.to_a[0]
                 case k
-                when 'puts'
+                when '__puts' #pseudo functions
                     v = eval_expr(v)
                     puts v
-                #pseudo functions
-                when 'for'
+                when '__for'
                     #check_arg(v, 2) or check_arg(v, :collection, :lambda)
                     v = eval_expr(v)
-                    collection, func = v[0], v[1]
-                    #only range
-                    collection = [*collection]
+                    collection, lambda = v[0], v[1]
+
+                    collection = to_collection(collection)
 
                     collection.each do |e|
-                        eval_lambda(func, e)
+                        eval_lambda(lambda, e)
                     end
-                when 'do'
+                when 'do' #lambda
                     expr
-                else
-                    puts "#{k} called!"
+                else #other functions
+                    eval_lambda(@function_table[k], eval_expr(v))
                 end
-            when ->(expr){ expr.class == String and expr =~ /\$(\w)+/ }
+            when ->(expr){ expr.class == String and expr =~ /\$(\w+)/ }
                 #local variables
                 var_name = $1
-                @context.get_local_variable(var_name)
+                @context.get_local_variable(var_name).tap do |val|
+                    #puts "get local variable! #{var_name} : #{val}"
+                end
             else
                 #primitives
                 expr
@@ -66,7 +94,6 @@ module XASH
 
         def eval(code)
             code = Roconv.convert(code)
-            pp code
             eval_expr(code)
         end
     end
